ab4674b0891f25412b47dcb7c32d276953c6e290
hongsheng <1634340724@qq.com>
2023-11-24 20:19:21 +0800

    引入网关依赖

---
+++ gq-backend-gateway/src/main/java/com/dgut/gq/www/gateway/GqBackendGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendGatewayApplication.class, args);
+    }
+
+}
---
+++ gq-backend-gateway/src/test/java/com/dgut/gq/www/gateway/GqBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqBackendGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
