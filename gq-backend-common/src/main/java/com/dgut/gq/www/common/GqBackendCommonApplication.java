eaaa9b5687d2bd30416ae7744152deb13a334794
hongsheng <1634340724@qq.com>
2023-11-24 15:15:38 +0800

    添加common模块依赖

---
+++ gq-backend-common/src/main/java/com/dgut/gq/www/common/GqBackendCommonApplication.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendCommonApplication.class, args);
+    }
+
+}
