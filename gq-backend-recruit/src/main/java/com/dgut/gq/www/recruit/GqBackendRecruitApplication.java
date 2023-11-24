81289c9ea0c8c1941f8d907395ab210fc567a99e
hongsheng <1634340724@qq.com>
2023-11-24 20:34:41 +0800

    添加招新模块

---
+++ gq-backend-recruit/src/main/java/com/dgut/gq/www/recruit/GqBackendRecruitApplication.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendRecruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendRecruitApplication.class, args);
+    }
+
+}
