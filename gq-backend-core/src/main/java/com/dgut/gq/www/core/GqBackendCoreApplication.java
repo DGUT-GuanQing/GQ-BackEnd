a7ca993c2a9db6e0129b9ce5f5cb2b6afe8f1cac
hongsheng <1634340724@qq.com>
2023-11-24 17:33:06 +0800

    添加核心模块依赖

---
+++ gq-backend-core/src/main/java/com/dgut/gq/www/core/GqBackendCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendCoreApplication.class, args);
+    }
+
+}
---
+++ gq-backend-core/src/test/java/com/dgut/gq/www/core/GqBackendCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqBackendCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
