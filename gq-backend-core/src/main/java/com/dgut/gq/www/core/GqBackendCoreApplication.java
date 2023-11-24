26fb06426d7aa7fac0fa0eef53bcecc3f5b0fd21
hongsheng <1634340724@qq.com>
2023-11-24 18:50:10 +0800

    添加核心模块配置类

--- gq-backend-core/src/main/java/com/dgut/gq/www/core/GqBackendCoreApplication.java
+++ gq-backend-core/src/main/java/com/dgut/gq/www/core/GqBackendCoreApplication.java
@@ -3,7 +3,7 @@ package com.dgut.gq.www.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.dgut.gq"})
 public class GqBackendCoreApplication {
 
     public static void main(String[] args) {
