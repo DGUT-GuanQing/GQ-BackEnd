c67476ca84da14ccd31a48765a594ce01c38be99
hongsheng <1634340724@qq.com>
2023-12-29 20:12:57 +0800

    拆分管理服务

---
+++ gq-backend-admin/src/main/java/com/dgut/gq/www/admin/GqBackendAdminApplication.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendAdminApplication.class, args);
+    }
+
+}
---
+++ gq-backend-admin/src/test/java/com/dgut/gq/www/admin/GqBackendAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgut.gq.www.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GqBackendAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
