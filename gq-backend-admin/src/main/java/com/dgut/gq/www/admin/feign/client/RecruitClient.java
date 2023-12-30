aaa26143e4da40af018f9bc112876d864bb98b01
hongsheng <1634340724@qq.com>
2023-12-31 02:38:54 +0800

    重构剩下的feign调用

--- gq-backend-admin/src/main/java/com/dgut/gq/www/admin/feign/client/RecruitClient.java
+++ gq-backend-admin/src/main/java/com/dgut/gq/www/admin/feign/client/RecruitClient.java
@@ -2,15 +2,12 @@ package com.dgut.gq.www.admin.feign.client;
 
 import com.dgut.gq.www.admin.model.dto.DepartmentDto;
 import com.dgut.gq.www.admin.model.dto.PositionDto;
-import com.dgut.gq.www.admin.model.vo.CurriculumVitaeVo;
 import com.dgut.gq.www.common.common.SystemJsonResponse;
-import io.swagger.annotations.*;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * Feign讲座模块客户端
+ * Feign招新模块客户端
  */
 @FeignClient(name = "gq-backend-recruit",contextId = "recruit")
 public interface RecruitClient {
