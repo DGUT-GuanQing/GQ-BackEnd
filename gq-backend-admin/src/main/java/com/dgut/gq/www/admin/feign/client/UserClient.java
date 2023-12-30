1bc192e5d95fc4be7e24c196c09c474eb2e0b1f4
hongsheng <1634340724@qq.com>
2023-12-30 12:45:47 +0800

    重构后台登陆远程调用

---
+++ gq-backend-admin/src/main/java/com/dgut/gq/www/admin/feign/client/UserClient.java
@@ -0,0 +1,21 @@
+package com.dgut.gq.www.admin.feign.client;
+
+import com.beust.jcommander.Parameter;
+import com.dgut.gq.www.common.model.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Feign用户模块客户端
+ */
+@FeignClient("gq-backend-core")
+public interface UserClient {
+
+    /**
+     * 远程调用根据用户名获取用户信息
+     * @return
+     */
+    @GetMapping("/feign-user/getUserByUserName")
+    User getUserByUserName(@RequestParam String userName);
+}
---
+++ gq-backend-core/src/main/java/com/dgut/gq/www/core/feign/server/UserServer.java
@@ -0,0 +1,29 @@
+package com.dgut.gq.www.core.feign.server;
+
+import com.dgut.gq.www.common.model.entity.User;
+import com.dgut.gq.www.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Feign用户模块服务端
+ */
+@RestController
+@RequestMapping("/feign-user")
+public class UserServer {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 远程调用根据用户名获取用户信息
+     * @return
+     */
+    @GetMapping("/getUserByUserName")
+    User getUserByUserName(@RequestParam String userName){
+        return userService.getUserByUsername(userName);
+    }
+}
