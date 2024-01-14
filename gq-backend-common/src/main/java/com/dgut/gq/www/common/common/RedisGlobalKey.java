122e8992f5a54f48cf3f1f95a4b75190c3a47704
hongsheng <1634340724@qq.com>
2024-01-14 17:30:12 +0800

    feat: 添加mq消费和发送失败的redis键

--- gq-backend-common/src/main/java/com/dgut/gq/www/common/common/RedisGlobalKey.java
+++ gq-backend-common/src/main/java/com/dgut/gq/www/common/common/RedisGlobalKey.java
@@ -26,31 +26,47 @@ public class RedisGlobalKey {
     /**
      * 未开始的讲座
      */
-    public  static  String UNSTART_LECTURE = "gq:lecture:unstart:";
+    public static String UNSTART_LECTURE = "gq:lecture:unstart:";
 
     /**
      * 锁
      */
-    public  static  String LOCK_USER = "gq:user:lock:";
+    public static String LOCK_USER = "gq:user:lock:";
 
     /**
      * 票的数量
      */
-    public static  String TICKET_NUMBER = "gq:lecture:ticket_number:";
+    public static String TICKET_NUMBER = "gq:lecture:ticket_number:";
 
     /**
      * 用户的信息
      */
-    public  static  String USER_MESSAGE = "gq:user:user_message:";
+    public  static String USER_MESSAGE = "gq:user:user_message:";
 
     /**
      * 海报类型
      */
-    public  static  String POSTER_TWEET =   "gq:common:poster_tweet:";
+    public static String POSTER_TWEET =   "gq:common:poster_tweet:";
 
     /**
      * 是否抢到票
      */
-    public  static  String IS_GRAB_TICKETS  = "gq:user:is_grab_tickets:";
+    public static String IS_GRAB_TICKETS  = "gq:user:is_grab_tickets:";
+
+    /**
+     * 发送消息到交换机失败
+     *
+     */
+    public static String SEND_EXCHANGE_FAIL = "gq:lecture:rob_ticket_error:send_exchange_fail:";
+
+    /**
+     *  消息从交换机路由到队列时报
+     */
+    public static String ROUTE_FAIL = "gq:lecture:rob_ticket_error:route_fail:";
+
+    /**
+     * 消费失败
+     */
+    public static String CONSUME_FAIL = "gq:lecture:rob_ticket_error:consume_fail:";
 
 }
