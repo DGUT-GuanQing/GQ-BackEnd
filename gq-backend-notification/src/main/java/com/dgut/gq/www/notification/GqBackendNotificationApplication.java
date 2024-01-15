e2eeac0fcf4590989835f6a35f3cf8e58eb80c77
xsinxcos <1465701009@qq.com>
2024-01-15 15:48:17 +0800

    feat:初始化消息推送模块

---
+++ gq-backend-notification/src/main/java/com/dgut/gq/www/notification/GqBackendNotificationApplication.java
@@ -0,0 +1,11 @@
+package com.dgut.gq.www.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqBackendNotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GqBackendNotificationApplication.class ,args);
+    }
+}
