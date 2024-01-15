11e579e78ff376c4774ce32c5cdb581e3ee08512
hongsheng <1634340724@qq.com>
2024-01-15 22:41:36 +0800

    bugfix: 删除镜像缓存

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,7 @@
 # 停止和删除容器
 docker-compose down
 
-# 清除并重新打包 common 依赖
-cd /opt/gq-backend/gq-backend-common
-mvn clean install
-cd ..
-
-# 对每个服务执行清除和打包
-# gq-backend-gateway
-cd /opt/gq-backend/gq-backend-gateway
 mvn clean package -DskipTests
-cd ..
-
-# gq-backend-core
-cd /opt/gq-backend/gq-backend-core
-mvn clean package -DskipTests
-cd ..
-
-# gq-backend-recruit
-cd /opt/gq-backend/gq-backend-recruit
-mvn clean package -DskipTests
-cd ..
-
-# gq-backend-admin
-cd /opt/gq-backend/gq-backend-admin
-mvn clean package -DskipTests
-cd ..
 
 # 构建并启动容器
 docker-compose build --no-cache
