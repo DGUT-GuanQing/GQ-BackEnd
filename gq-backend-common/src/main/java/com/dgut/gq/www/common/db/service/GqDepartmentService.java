765a285b8cdc732d4ec71ec67006022523fd220e
honshen <1634340724@qq.com>
2024-12-25 00:24:10 +0800

    style：简历模块调整

--- gq-backend-common/src/main/java/com/dgut/gq/www/common/db/service/GqDepartmentService.java
+++ gq-backend-common/src/main/java/com/dgut/gq/www/common/db/service/GqDepartmentService.java
@@ -3,5 +3,9 @@ package com.dgut.gq.www.common.db.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.dgut.gq.www.common.db.entity.Department;
 
+import java.util.List;
+
 public interface GqDepartmentService extends IService<Department> {
+
+    List<Department> getAll();
 }
--- gq-backend-common/src/main/java/com/dgut/gq/www/common/db/service/GqPositionService.java
+++ gq-backend-common/src/main/java/com/dgut/gq/www/common/db/service/GqPositionService.java
@@ -3,5 +3,9 @@ package com.dgut.gq.www.common.db.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.dgut.gq.www.common.db.entity.Position;
 
+import java.util.List;
+
 public interface GqPositionService extends IService<Position> {
+
+    List<Position> getByDepartmentId(String departmentId);
 }
