d73f7587dcd9dc6f87caef0e8c35ad07a4b3c0a5
hongshen <101470186+interca@users.noreply.github.com>
2025-01-01 02:45:08 +0800

    Revert "style: 抢票代码调整"
    This reverts commit 4a8e9a9ce3d9e0ca7b51c9e8251ed06590f4bee0.

--- gq-backend-core/src/main/java/com/dgut/gq/www/core/service/impl/LectureServiceImpl.java
+++ gq-backend-core/src/main/java/com/dgut/gq/www/core/service/impl/LectureServiceImpl.java
@@ -163,12 +163,14 @@ public class LectureServiceImpl implements LectureService {
                 return SystemJsonResponse.fail(GlobalResponseCode.OPERATE_FAIL.getCode(), "抢票失败");
             }
         } catch (InterruptedException e) {
-            log.error("LectureServiceImpl robTicket 获取锁失败 openid = {}, lectureId = {}", openid, lectureId, e);
+            log.error("LectureServiceImpl robTicket 抢票失败 openid = {}, lectureId = {}", openid, lectureId);
             return SystemJsonResponse.fail(GlobalResponseCode.OPERATE_FAIL.getCode(), "抢票失败");
         }
         try {
             //执行lua脚本
-            Long execute = stringRedisTemplate.execute(SECKILL_SCRIPT, Collections.emptyList(), lectureId, openid);
+            Long execute = stringRedisTemplate.execute(
+                    SECKILL_SCRIPT, Collections.emptyList(), lectureId, openid);
+            assert execute != null;
             int re = execute.intValue();
             String msg = re == 1 ? "抢票成功" : "抢票失败";
             log.info("LectureServiceImpl robTicket openid = {}, lectureId = {}, msg = {}", openid, lectureId, msg);
@@ -176,8 +178,6 @@ public class LectureServiceImpl implements LectureService {
                 return SystemJsonResponse.fail(GlobalResponseCode.OPERATE_FAIL.getCode(), msg);
             }
             SendMsg(openid, lectureId);
-        } catch (Exception e) {
-            log.info("LectureServiceImpl robTicket 抢票失败 openid = {}, lectureId = {}", openid, lectureId, e);
         } finally {
             lock.unlock();
         }
