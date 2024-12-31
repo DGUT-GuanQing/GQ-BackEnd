5cf35238231567a246d5601bc62330327ba0b58c
honshen <1634340724@qq.com>
2024-12-31 19:22:22 +0800

    style: 增加异常处理

--- gq-backend-recruit/src/main/java/com/dgut/gq/www/recruit/service/impl/RecruitmentServiceImpl.java
+++ gq-backend-recruit/src/main/java/com/dgut/gq/www/recruit/service/impl/RecruitmentServiceImpl.java
@@ -177,7 +177,6 @@ public class RecruitmentServiceImpl implements RecruitmentService {
     public SystemJsonResponse getAllCurriculumVitae(int page, int pageSize, String departmentId, Integer term) {
         Page<CurriculumVitae> pageInfo = gqCurriculumVitaeService.pageByDepartmentIdAndTerm(page, pageSize, departmentId, term);
         List<CurriculumVitae> records = pageInfo.getRecords();
-        Integer count = (int) pageInfo.getTotal();
         log.info("RecruitmentServiceImpl getAllCurriculumVitae CurriculumVitaes = {}", JSONUtil.toJsonStr(records));
         Map<String, User> userMap = gqUserService.getByOpenIds(
                 records.stream()
@@ -215,7 +214,7 @@ public class RecruitmentServiceImpl implements RecruitmentService {
                 }).collect(Collectors.toList());
         log.info("RecruitmentServiceImpl getAllCurriculumVitae departmentId = {}, term = {}, curriculumVitaeVoList = {}", departmentId, term, JSONUtil.toJsonStr(curriculumVitaeVoList));
 
-        return SystemJsonResponse.success(new SystemResultList(curriculumVitaeVoList, count));
+        return SystemJsonResponse.success(new SystemResultList<>(curriculumVitaeVoList, (int) pageInfo.getTotal()));
     }
 
     /**
